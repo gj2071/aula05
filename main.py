8788958592fdee6ddbed7bdbaa5693c2ff0d554c
gj2071 <143042340+gj2071@users.noreply.github.com>
2023-10-07 12:36:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,222 @@
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+for i in matriz: 
+   for n in i: 
+       print(n)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+# # dicionario
+# dados  =  {
+# 'nome': 'Julio',
+# 'idade': 18,
+# 'endereco': 'rua 10'
+# }
+
+# for i1,i2 in dados.items(): 
+#     print(f'{i1} : {i2}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # palavra  = 'Python'
+# # for letra in palavra: 
+# #     print(letra)
+
+
+
+
+
+
+
+
+# # **Exercício 1:** Escreva um programa que use a função `range()` para gerar os números pares de 2 a 20 e, em seguida, imprima cada número.
+
+# # for i in range(2,21,2):
+# #      print(i)
+
+
+
+# # **Exercício 2:** Escreva um programa que use a função `range()` para gerar os múltiplos de 5 em 5 a 50 e, em seguida, calcule e imprima a soma desses múltiplos.
+# # for n in range(5,51,5):
+# #     print(n)
+# multiplus = range(5,51,5)
+# s = len(multiplus)
+# print(s)
+
+
+
+
+# # **Exercício 3:** Escreva um programa que use a função `type()` para verificar o tipo de uma variável.
+
+# palavra  = 'Text'
+# tipo = type(palavra)
+# print(tipo)
+
+
+
+# # **Exercício 4:** Escreva um programa que use a função `print()` para imprimir uma mensagem de saudação personalizada, incluindo o nome do usuário.
+
+# # print('Ola', name , '=' , 'Julio')
+
+# # **Exercício 5:** Escreva um programa que use a função `range()` para gerar os números ímpares de 1 a 10 e, em seguida, calcule e imprima a média desses números.
+
+
+# for i in range(1,11,2): 
+#      print(i)
+#      f = i
+  
+# n =  range(1,11,2)
+# s  =  sum(n)/len(n)
+# print(f'a média de {n} é {s}')
+
+
+
+
+
+
+
+
+# # 5 Escreva um programa que use a função range() para gerar os múltiplos de 5 em 5 a 50 e, em seguida, calcule e imprima a soma desses múltiplos.
+
+# # r= range(1,5)
+# # s = sum(r)
+# # print(s)
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # lista0 = [123,4554,5656]
+# # lista = [56,566,7878,15,1,0,89,10]
+# # organize = sorted(lista0+lista)
+# # print(organize)
+
+
+
+
+
+
+
+
+
+
+# # lista = [2.8,0,145,1.4]
+# # text = [ 'a','b','c']
+
+# # maxi = max(lista)
+# # print(maxi)
+
+
+
+
+
+
+
+
+
+
+
+# # lista = [1,6,6]
+# # soma  =  sum(lista)
+# # print('a soma de tudo é: ',soma)
+
+
+
+
+
+
+
+
+# # n = range (1,3)
+# # print(n)
+
+
+
+
+
+
+
+
+
+
+# # print()
+# # cidade  = 'Guarulhos'
+# # print(cidade,'\n')
+# # frag = list(cidade)
+# # # print(frag)
+
+# # tr = tuple(frag)
+# # print(tr)
+
+
+
+
+
+
+
+
+# # cpf =  '4523323423'
+# # n = float(cpf)
+# # print(n)
+# # # print(n+5)
+
+
+
+
+
+
+# # x  = '1234546'
+# # print(len(x))
+
+
+
+
+
+
+
+
+
+# # x = 'terra'
+# # dado = x
+# # print(type(dado))
